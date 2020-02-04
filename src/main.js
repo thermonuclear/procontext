05dc9b735040b414440d571974a7afecc842391b
Anatoliy Smilik <cvbkbr@gmail.com>
2020-02-04 09:47:49 +0500

    del Loader

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
-import Loader from './components/app/Loader'
 
 Vue.config.productionTip = false;
 
-Vue.component('Loader', Loader)
 /* eslint-disable */
 new Vue({
   store,
